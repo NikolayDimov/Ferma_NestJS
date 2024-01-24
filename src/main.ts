a30eedb873bb2a4f25fac2921e6796900f31b615
Nikolay Dimov <dimovv.nikolay@gmail.com>
2024-01-24 15:17:54 +0200

    add CORS in main.ts to acsept request from React

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+// import { NestFactory } from "@nestjs/core";
+// import { AppModule } from "./app.module";
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(3000);
+// }
+// bootstrap();
+
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Enable CORS for all routes
+  app.enableCors({
+    origin: "http://localhost:5173", // Replace with your React app's URL
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
+
 bootstrap();
